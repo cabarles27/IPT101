4674d4ea0ed5e89783ccd05120f63eab4f985e57
cabarles27 <143572741+cabarles27@users.noreply.github.com>
2025-10-22 20:11:04 +0800

    Add files via upload

---
+++ wpf/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+		string[] names = { "kaye", "jane", "Kai" };
+		int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+
+		foreach (string name in names)
+		{
+			Console.Write(name + " ");
+		}
+		Console.WriteLine();
+		foreach (int number in num)
+		{
+			Console.Write(number + " ");
+		}
+	}
+}
