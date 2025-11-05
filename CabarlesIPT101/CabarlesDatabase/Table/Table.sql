38e3208993776c97ca3c9d4376df771a229a8607
cabarles27 <143572741+cabarles27@users.noreply.github.com>
2025-11-05 15:36:31 +0800

    Add files via upload

---
+++ CabarlesIPT101/CabarlesDatabase/Table/Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[POS]
+(
+	
+    [SaleId] NVARCHAR(10) NOT NULL, 
+    [ProductName] NVARCHAR(10) NULL, 
+    [Quantity] INT NULL, 
+    [Price] INT NULL, 
+    [TotalAmount] INT NULL
+)
---
+++ CabarlesIPT101/CabarlesDomain/Commands/IUpdateYouTubeViewerCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cabarles_IPT.CabarlesDomain.Models;
+
+namespace Cabarles_IPT.CabarlesDomain.Commands
+{
+    public interface IUpdateYouTubeViewerCommand
+    {
+        Task Execute(YouTubeViewer youTubeViewer);
+    }
+}
