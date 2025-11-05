d4d6a5717968dde9ece1ea11f4cc6625fe20dd5c
cabarles27 <143572741+cabarles27@users.noreply.github.com>
2025-11-05 15:39:12 +0800

    Add files via upload

---
+++ CabarlesIPT101/CabarlesYoutubeViewer/Commands/CommandBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Cabarles_IPT.CabarlesYoutubeViewer.Commands
+{
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public virtual bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public abstract void Execute(object parameter);
+
+        protected virtual void OnCanExecutedChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
---
+++ CabarlesIPT101/CabarlesYoutubeViewer/ViewModels/AddYouTubeViewerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Cabarles_IPT.CabarlesYoutubeViewer.Commands;
+using Cabarles_IPT.CabarlesYoutubeViewer.Stores;
+
+namespace Cabarles_IPT.CabarlesYoutubeViewer.ViewModels
+{
+    public class AddYouTubeViewerViewModel : ViewModelBase
+    {
+        public YouTubeViewerDetailsFormViewModel YouTubeViewerDetailsFormViewModel { get; }
+
+        public AddYouTubeViewerViewModel(YouTubeViewersStore youTubeViewersStore, ModalNavigationStore modalNavigationStore)
+        {
+            ICommand submitCommand = new AddYouTubeViewerCommand(this, youTubeViewersStore, modalNavigationStore);
+            ICommand cancelCommand = new CloseModalCommand(modalNavigationStore);
+            YouTubeViewerDetailsFormViewModel = new YouTubeViewerDetailsFormViewModel(submitCommand, cancelCommand);
+        }
+    }
+}
