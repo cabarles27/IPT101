373d40bd07c6787183a462ea2ce36cc9605d2b5f
cabarles27 <143572741+cabarles27@users.noreply.github.com>
2025-11-19 10:18:45 +0800

    Add files via upload

---
+++ CabarlesIPT101/CabarlesYoutubeViewer/Commands/OpenAddYouTubeViewerCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Cabarles_IPT.CabarlesYoutubeViewer.Stores;
+using Cabarles_IPT.CabarlesYoutubeViewer.ViewModels;
+
+namespace Cabarles_IPT.CabarlesYoutubeViewer.Commands
+{
+    public class OpenAddYouTubeViewerCommand : CommandBase
+    {
+        private readonly YouTubeViewersStore _youTubeViewersStore;
+        private readonly ModalNavigationStore _modalNavigationStore;
+
+        public OpenAddYouTubeViewerCommand(YouTubeViewersStore youTubeViewersStore, ModalNavigationStore modalNavigationStore)
+        {
+            _youTubeViewersStore = youTubeViewersStore;
+            _modalNavigationStore = modalNavigationStore;
+        }
+
+        public override void Execute(object parameter)
+        {
+            AddYouTubeViewerViewModel addYouTubeViewerViewModel = new AddYouTubeViewerViewModel(_youTubeViewersStore, _modalNavigationStore);
+            _modalNavigationStore.CurrentViewModel = addYouTubeViewerViewModel;
+        }
+    }
+}
---
+++ CabarlesIPT101/CabarlesYoutubeViewer/ViewModels/EditYouTubeViewerViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Cabarles_IPT.CabarlesYoutubeViewer.Commands;
+using Cabarles_IPT.CabarlesDomain.Models;
+using Cabarles_IPT.CabarlesYoutubeViewer.Stores;
+
+namespace Cabarles_IPT.CabarlesYoutubeViewer.ViewModels
+{
+    public class EditYouTubeViewerViewModel : ViewModelBase
+    {
+        public Guid YouTubeViewerId { get; }
+
+        public YouTubeViewerDetailsFormViewModel YouTubeViewerDetailsFormViewModel { get; }
+
+        public EditYouTubeViewerViewModel(YouTubeViewer youTubeViewer, YouTubeViewersStore youTubeViewersStore, ModalNavigationStore modalNavigationStore)
+        {
+            YouTubeViewerId = youTubeViewer.Id;
+
+            ICommand submitCommand = new EditYouTubeViewerCommand(this, youTubeViewersStore, modalNavigationStore);
+            ICommand cancelCommand = new CloseModalCommand(modalNavigationStore);
+            YouTubeViewerDetailsFormViewModel = new YouTubeViewerDetailsFormViewModel(submitCommand, cancelCommand)
+            { 
+                Username = youTubeViewer.Username,
+                IsSubscribed = youTubeViewer.IsSubscribed,
+                IsMember = youTubeViewer.IsMember,
+            };
+        }
+    }
+}
