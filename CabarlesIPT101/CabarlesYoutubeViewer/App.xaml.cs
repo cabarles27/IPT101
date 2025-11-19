71d8d195f3b18450fe1f974d6c8ec6f539bdba28
cabarles27 <143572741+cabarles27@users.noreply.github.com>
2025-11-19 09:55:55 +0800

    Add files via upload

---
+++ CabarlesIPT101/CabarlesYoutubeViewer/App.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Cabarles_IPT.CabarlesDomain.Commands;
+using Cabarles_IPT.CabarlesDomain.Queries;
+using Cabarles_IPT.CabarlesFramework;
+using Cabarles_IPT.CabarlesFramework.Commands;
+using Cabarles_IPT.CabarlesFramework.Queries;
+using Cabarles_IPT.CabarlesYoutubeViewer.Stores;
+using Cabarles_IPT.CabarlesYoutubeViewer.ViewModels;
+using Cabarles_IPT.CabarlesYoutubeViewer.HostBuilders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cabarles_IPT.CabarlesYoutubeViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .AddDbContext()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<IGetAllYouTubeViewersQuery, GetAllYouTubeViewersQuery>();
+                    services.AddSingleton<ICreateYouTubeViewerCommand, CreateYouTubeViewerCommand>();
+                    services.AddSingleton<IUpdateYouTubeViewerCommand, UpdateYouTubeViewerCommand>();
+                    services.AddSingleton<IDeleteYouTubeViewerCommand, DeleteYouTubeViewerCommand>();
+
+                    services.AddSingleton<ModalNavigationStore>();
+                    services.AddSingleton<YouTubeViewersStore>();
+                    services.AddSingleton<SelectedYouTubeViewerStore>();
+
+                    services.AddTransient<YouTubeViewersViewModel>(CreateYouTubeViewersViewModel);
+                    services.AddSingleton<MainViewModel>();
+
+                    services.AddSingleton<MainWindow>((services) => new MainWindow()
+                    {
+                        DataContext = services.GetRequiredService<MainViewModel>()
+                    });
+                })
+                .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            YouTubeViewersDbContextFactory youTubeViewersDbContextFactory = 
+                _host.Services.GetRequiredService<YouTubeViewersDbContextFactory>();
+            using(YouTubeViewersDbContext context = youTubeViewersDbContextFactory.Create())
+            {
+                // Apply migrations to ensure database is up to date
+                context.Database.Migrate();
+            }
+
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.StopAsync();
+            _host.Dispose();
+
+            base.OnExit(e);
+        }
+
+        private YouTubeViewersViewModel CreateYouTubeViewersViewModel(IServiceProvider services)
+        {
+            return YouTubeViewersViewModel.LoadViewModel(
+                services.GetRequiredService<YouTubeViewersStore>(),
+                services.GetRequiredService<SelectedYouTubeViewerStore>(),
+                services.GetRequiredService<ModalNavigationStore>());
+        }
+    }
+}
